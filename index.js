9c7e958325bfb6e4bcb8b27f438a99aa6c28e42d
Pukar Sharma <sharma.pukar99@gmail.com>
2023-12-13 14:08:20 +0545

    creting hello.txtfile

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// const app = require('./app')
-// console.log(app.z())
-const arr = [1,2,3,3,2,4,5,6,7,5,3]
-let result = arr.filter((item)=>{
-    return item 
-})
-console.log(result)
+// // const app = require('./app')
+// // console.log(app.z())
+// const arr = [1,2,3,3,2,4,5,6,7,5,3]
+// let result = arr.filter((item)=>{
+//     return item 
+// })
+// console.log(result)
+const fs = require('fs');
+fs.writeFileSync("hello.txt"," i am very happy with you happy to your coding journey thank me later")
