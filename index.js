db094c534724502230ca6a87352de22681226efa
Pukar Sharma <sharma.pukar99@gmail.com>
2023-12-14 18:36:26 +0545

    all about package.json

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-// // const app = require('./app')
-// // console.log(app.z())
-// const arr = [1,2,3,3,2,4,5,6,7,5,3]
-// let result = arr.filter((item)=>{
-//     return item 
-// })
-// console.log(result)
-const fs = require('fs');
-fs.writeFileSync("Readmeguys.txt"," This is the second file that i created here so thank me later you went here checking all my works ok ")
+// // // const app = require('./app')
+// // // console.log(app.z())
+// // const arr = [1,2,3,3,2,4,5,6,7,5,3]
+// // let result = arr.filter((item)=>{
+// //     return item 
+// // })
+// // console.log(result)
+// // const fs = require('fs');
+// // fs.writeFileSync("Readmeguys.txt"," This is the second file that i created here so thank me later you went here checking all my works ok ")
+
+
+
+// const fs = require('fs');
+// console.log("->>",__filename);
+// fs.writeFileSync("code.txt", "i am checking the global and non global module")
+const colors = require('colors')
+console.log("hello ".red);
+console.log("hello ".green);
+console.log("hello ".bgBlack);
